f6ac582cf8e8739e1dfc85d54510afea1a81e6d4
luis-jmtz <luis.jmtz2002@gmail.com>
2025-08-01 21:00:20 -0500

    going to test modifying ancestry traits

---
+++ data_modifiers.py
@@ -0,0 +1,33 @@
+import pandas as pd
+
+def append_ancestries(ancestry_name,size,core_traits,secondary_traits):
+	path = fr"data\Ancestries.tsv"
+	pass
+
+def append_classes(class_name):
+	path = fr"data\classID_Class.tsv"
+	df = pd.read_csv(path,sep = '\t')
+
+	last_id = int(df['classID'].iloc[-1])
+	# print(last_id)
+	new_id_value = last_id + 1
+
+	df.loc[len(df)] = [new_id_value, class_name]
+	df.to_csv("data\classID_Class.tsv", index=False,sep = '\t')
+	print(df)
+
+
+def append_ancestry_traits(trait_name,isCore,trait_description):
+	path = fr"data\Ancestry_Traits.tsv"
+	df = pd.read_csv(path,sep = '\t')
+
+	last_id = int(df['classID'].iloc[-1])
+	# print(last_id)
+	new_id_value = last_id + 1
+
+	df.loc[len(df)] = [new_id_value, trait_name, isCore,trait_description]
+	df.to_csv("data\Ancestry_Traits.tsv", index=False,sep = '\t')
+	print(df)
+
+
+append_ancestry_traits("Name",0,"Description")
