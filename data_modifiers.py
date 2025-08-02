5df49ec08b2c1470fc225de359d79ec3705b1136
luis-jmtz <luis.jmtz2002@gmail.com>
2025-08-01 21:11:27 -0500

    Ancestry append works. Mod. Ancestries to have python lists

--- data_modifiers.py
+++ data_modifiers.py
@@ -1,8 +1,16 @@
 import pandas as pd
 
-def append_ancestries(ancestry_name,size,core_traits,secondary_traits):
+def append_ancestries(ancestry_name,size,speed, core_traits,secondary_traits):
 	path = fr"data\Ancestries.tsv"
-	pass
+	df = pd.read_csv(path,sep = '\t')
+
+	last_id = int(df['ancestryID'].iloc[-1])
+	# print(last_id)
+	new_id_value = last_id + 1
+
+	df.loc[len(df)] = [new_id_value, ancestry_name,size, speed, core_traits,secondary_traits]
+	df.to_csv("data\Ancestries.tsv", index=False,sep = '\t')
+	print(df)
 
 def append_classes(class_name):
 	path = fr"data\classID_Class.tsv"
@@ -30,3 +38,4 @@ def append_ancestry_traits(trait_name,isCore,trait_description):
 	print(df)
 
 
+append_ancestries("New",[-1,0],6,[2,3],[4,5])
