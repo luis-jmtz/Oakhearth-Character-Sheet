dfc50dd95548bcfc3cf849168b6146a481f40cfe
luis-jmtz <luis.jmtz2002@gmail.com>
2025-08-11 18:11:26 -0500

    Ancestries Page Done

--- pages/1_Ancestries.py
+++ pages/1_Ancestries.py
@@ -30,21 +30,44 @@ def parse_common(value):
 	else:
 		return "(Uncommon)"
 
+def parse_traits(traits_list):
+
+	output_text = ""
+
+	for index, row in ancestry_traits.iterrows():
+		if ancestry_traits["traitID"].iloc[index] in traits_list:
+			trait_name = ancestry_traits["Name"].iloc[index]
+			trait_descript = ancestry_traits["Description"].iloc[index]
+			output_text += f"{trait_name}: {trait_descript} \n\n"
+			
+
+	return output_text
 
 for index, row in ancestries.iterrows():
 	ancestry = ancestries["Ancestry"].iloc[index]
-	st.write(f"## {ancestry}")
-
+	
 	isCommon = int(ancestries["isCommon"].iloc[index])
 	isCommon = parse_common(isCommon)
-
-	st.write(f"{isCommon}")
 
 	size = ast.literal_eval(ancestries["Size"].iloc[index])
 	size_text = parse_size(size)
 
 	speed = ancestries["Speed"].iloc[index]
 
-	st.write(f"Size: {size_text}")
-	st.write(f"Speed: {speed}")
+	#--- Parse Traits ---
+
+	core_traits = ast.literal_eval(ancestries["Core_Traits"].iloc[index])
+	core_traits = parse_traits(core_traits)
+
+	secondary_traits = ast.literal_eval(ancestries["Secondary_Traits"].iloc[index])
+	secondary_traits = parse_traits(secondary_traits)
+
+	#--- Results ----
+	st.write(f"## {ancestry}")
+	st.write(f"{isCommon}")
+	st.markdown(f"*Size*: {size_text} \n\n*Speed*: {speed}")
+	st.write("#### Core Traits")
+	st.markdown(core_traits)
+	st.write("#### Secondary Traits")
+	st.markdown(secondary_traits)
 
