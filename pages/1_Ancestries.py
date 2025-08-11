623049fda8790a4fcf36f031f2eafc4e5e5f550a
luis-jmtz <luis.jmtz2002@gmail.com>
2025-08-11 17:16:27 -0500

    successfully parse ancestry name and size

--- pages/1_Ancestries.py
+++ pages/1_Ancestries.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import ast
 
 # ----- Load Data ---- 
 st.session_state.ancestries = pd.read_csv(fr"data\Ancestries.tsv",sep = '\t')
@@ -9,6 +10,33 @@ ancestry_traits = st.session_state.ancestry_traits
 
 st.title("Ancestries")
 
-for row in ancestries['Ancestry']:
-	st.write(f"## {row}")
-	# print(row)
+def parse_size(size_list):
+	size_ouput = ""
+
+	for index in size_list:
+		if size_list[index] == 0:
+			size_ouput += "Medium"
+			if len(size_list) > 1:
+				size_ouput +=", "
+		if size_list[index] == -1:
+			size_ouput += "Small"
+
+	return size_ouput
+
+
+
+for index, row in ancestries.iterrows():
+	ancestry = ancestries["Ancestry"].iloc[index]
+	st.write(f"## {ancestry}")
+
+	size = ast.literal_eval(ancestries["Size"].iloc[index])
+
+	size_text = parse_size(size)
+
+	st.write(f"**Size**: {size_text}")
+
+
+# for row in ancestries['Ancestry']:
+# 	st.write(f"## {row}")
+# 	st.write(f"Size {ancestries['Size']}")
+# 	# print(row)
