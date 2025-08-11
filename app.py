84a945d610a4d83e41e125157971cbab2613ff66
luis-jmtz <luis.jmtz2002@gmail.com>
2025-08-10 22:21:14 -0500

    removed data_loader

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 import streamlit as st
 import json
-import os
-import ast
-import sqlite3
+import pandas as pd
+# import os
+# import ast
+# import sqlite3
 
-st.title("Oakhearth Character Creator")
+# st.title("Oakhearth Character Creator")
+
+# ------- Load Data -------
+ancestries = pd.read_csv(fr"data\Ancestries.tsv",sep = '\t')
+ancestry_traits = pd.read_csv(fr"data\Ancestry_Traits.tsv",sep="\t")
+classes = pd.read_csv(fr"data\classID_Class.tsv",sep="\t")
 
 with open(fr'character_template.json', 'r') as file:
     char_data = json.load(file)
     # print(char_data)
-    
-# print(char_data["Level"])
