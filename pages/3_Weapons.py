7a327b8408b015cc6536c3d775ec2d9a2c12356d
luis-jmtz <luis.jmtz2002@gmail.com>
2025-08-31 17:09:56 -0500

    no sig progress

--- pages/3_Weapons.py
+++ pages/3_Weapons.py
@@ -5,46 +5,52 @@ import json
 
 st.title("Weapons")
 
-weapons = pd.read_csv(r"data\Equipment\Weapons.tsv", sep="\t")
+st.session_state.weapons = pd.read_csv(r"data\Equipment\Weapons.tsv", sep="\t")
 
 with open('data\Equipment\weapon_dict.json', 'r') as f:
-    dicitonary = json.load(f)
+    st.session_state.dicitonary = json.load(f)
     
-
-types = {}
-styles = {}
-dmg_types = {}
-basic_properties = {}
-complex_properties = {}
-
-
-attributes = [types, styles, dmg_types,basic_properties,complex_properties]
+weapon_types = st.session_state.dicitonary["type"]
+weapon_styles = st.session_state.dicitonary["styles"]
+damage_types = st.session_state.dicitonary["dmg_types"]
+basic_properties = st.session_state.dicitonary["basic_properties"]
+complex_properties = st.session_state.dicitonary["complex_properties"]
 
 
+for value in weapon_types.values():
+    temp = value.replace("_", " ")
+    st.write(temp)
 
-for key,value in dicitonary.items():    
-	st.write(f"{key}, {value}")
-    
-         
-	
+
+
+
 
 
 col1, col2, col3, col4 = st.columns(4)
 
 with col1:
-    pass
+    Wtype = st.selectbox(
+        "Weapon Type",
+        weapon_types.values()
+    )
 
 with col2:
-    pass
+    Dtype = st.selectbox(
+        "Damage Type",
+        damage_types.values()
+    )
 
 with col3:
-    pass
+    Bproperties = st.selectbox(
+        "Basic Properties",
+        basic_properties.values()
+    )
 
 with col4:
     pass
 
 
-st.dataframe(weapons)
+st.dataframe(st.session_state.weapons)
 
 
 display_table = ""
